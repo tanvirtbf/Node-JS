4216bf5098760d9b6e5d9b3e8f170221a5384fe7
tanvirtbf <tanver018765@gmail.com>
2024-09-28 16:35:16 +0600

    response create and send in clinet

--- Node 2/Request & Response/app.js
+++ Node 2/Request & Response/app.js
@@ -4,7 +4,15 @@ const server = http.createServer((req,res)=>{
   console.log(req.url)
   console.log(req.method)
   console.log(req.headers)
-  process.exit()
+
+  // res.setHeader('Content-Type','json')
+  res.setHeader('Content-Type', 'text/html')
+  res.write('<html>')
+  res.write('<head><title>Complete Coding</title></head>')
+  res.write('<body><h1>Hello, I am Tanvir</h1></body>')
+  res.write('</html>')
+  res.end()
+
 })
 
 const PORT = 3000
