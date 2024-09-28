fc1840002fff80a949c482fb5143f1e0096f0cd4
tanvirtbf <tanver018765@gmail.com>
2024-09-28 16:12:45 +0600

    request

--- Node 2/Request & Response/app.js
+++ Node 2/Request & Response/app.js
@@ -1,7 +1,9 @@
 const http = require('http')
 
 const server = http.createServer((req,res)=>{
-  console.log(req)
+  console.log(req.url)
+  console.log(req.method)
+  console.log(req.headers)
   process.exit()
 })
 
