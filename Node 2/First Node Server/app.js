e0d0c36f8b59f9bd3a3f36cec64127687542147d
tanvirtbf <tanver018765@gmail.com>
2024-09-28 15:10:29 +0600

    server listen

--- Node 2/First Node Server/app.js
+++ Node 2/First Node Server/app.js
@@ -2,8 +2,12 @@
 
 const http = require('http')
 
-function requestListener(req,res){
+const server = http.createServer((req,res)=>{
   console.log(req)
-}
+})
 
-http.createServer(requestListener)
+const PORT = 3000
+
+server.listen(PORT, ()=> {
+  console.log(`Server Running at http://localhost:${PORT}`)
+})
