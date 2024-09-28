091fd57769e6aef412aea3bf975dc99a33d1dd1d
tanvirtbf <tanver018765@gmail.com>
2024-09-28 17:23:42 +0600

    let's start recieve data from user

--- Node 2/Request & Response/app.js
+++ Node 2/Request & Response/app.js
@@ -5,28 +5,32 @@ const server = http.createServer((req, res) => {
   console.log(req.method)
   console.log(req.headers)
 
-  if (req.url === "/") {
-    // res.setHeader('Content-Type','json')
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>Home Page</title></head>");
-    res.write("<body><h1>Hello, I am Tanvir</h1></body>");
-    res.write("</html>");
-    return res.end();
-  } else if (req.url === "/products") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>Products Page</title></head>");
-    res.write("<body><h1>Hello, This is Product Page</h1></body>");
-    res.write("</html>");
-    return res.end();
-  }
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>Error Page</title></head>");
-  res.write("<body><h1>404 Not Found</h1></body>");
-  res.write("</html>");
-  res.end();
+  
+
+
+
+// if (req.url === "/") {
+  //   // res.setHeader('Content-Type','json')
+  //   res.setHeader("Content-Type", "text/html");
+  //   res.write("<html>");
+  //   res.write("<head><title>Home Page</title></head>");
+  //   res.write("<body><h1>Hello, I am Tanvir</h1></body>");
+  //   res.write("</html>");
+  //   return res.end();
+  // } else if (req.url === "/products") {
+  //   res.setHeader("Content-Type", "text/html");
+  //   res.write("<html>");
+  //   res.write("<head><title>Products Page</title></head>");
+  //   res.write("<body><h1>Hello, This is Product Page</h1></body>");
+  //   res.write("</html>");
+  //   return res.end();
+  // }
+  // res.setHeader("Content-Type", "text/html");
+  // res.write("<html>");
+  // res.write("<head><title>Error Page</title></head>");
+  // res.write("<body><h1>404 Not Found</h1></body>");
+  // res.write("</html>");
+  // res.end();
 });
 
 const PORT = 3000;
