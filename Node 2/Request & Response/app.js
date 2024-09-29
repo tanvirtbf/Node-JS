8dd8b653af8f40305cf3949a4323694e3df966d2
tanvirtbf <tanver018765@gmail.com>
2024-09-29 10:06:25 +0600

    form data parse another route

--- Node 2/Request & Response/app.js
+++ Node 2/Request & Response/app.js
@@ -10,42 +10,42 @@ const server = http.createServer((req, res) => {
     res.write('<html>')
     res.write('<head><title>Home Page</title></head>')
     res.write('<body><h1>Enter Your Details</h1></body>')
-    res.write('<form>')
-    res.write('<input type="text" name="username" placeholder="Enter your name">')
+    res.write('<form action="/submit-details" method="POST">')
+    res.write('<input type="text" name="username" placeholder="Enter your name"><br />')
     res.write('<label for="male">Male</label>')
     res.write('<input type="radio" id="male" name="gender" value="male" >')
     res.write('<label for="female">Female</label>')
     res.write('<input type="radio" id="female" name="gender" value="female" >')
-    res.write('<input type="submit" value="Submit" >')
+    res.write('<br /><input type="submit" value="Submit" >')
     res.write('</form>')
     res.write('</html>')
-    res.end()
+    return res.end()
   }
 
 
 
-// if (req.url === "/") {
-  //   // res.setHeader('Content-Type','json')
-  //   res.setHeader("Content-Type", "text/html");
-  //   res.write("<html>");
-  //   res.write("<head><title>Home Page</title></head>");
-  //   res.write("<body><h1>Hello, I am Tanvir</h1></body>");
-  //   res.write("</html>");
-  //   return res.end();
-  // } else if (req.url === "/products") {
-  //   res.setHeader("Content-Type", "text/html");
-  //   res.write("<html>");
-  //   res.write("<head><title>Products Page</title></head>");
-  //   res.write("<body><h1>Hello, This is Product Page</h1></body>");
-  //   res.write("</html>");
-  //   return res.end();
-  // }
-  // res.setHeader("Content-Type", "text/html");
-  // res.write("<html>");
-  // res.write("<head><title>Error Page</title></head>");
-  // res.write("<body><h1>404 Not Found</h1></body>");
-  // res.write("</html>");
-  // res.end();
+if (req.url === "/") {
+    // res.setHeader('Content-Type','json')
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Home Page</title></head>");
+    res.write("<body><h1>Hello, I am Tanvir</h1></body>");
+    res.write("</html>");
+    return res.end();
+  } else if (req.url === "/products") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Products Page</title></head>");
+    res.write("<body><h1>Hello, This is Product Page</h1></body>");
+    res.write("</html>");
+    return res.end();
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>Error Page</title></head>");
+  res.write("<body><h1>404 Not Found</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 const PORT = 3000;
