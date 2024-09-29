2d519d9bc75e45a87daca038674139aa7df1abd1
tanvirtbf <tanver018765@gmail.com>
2024-09-29 11:22:59 +0600

    new project start

--- Node 2/Request & Response/app.js
+++ Node 2/Request & Response/app.js
@@ -1,11 +1,15 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   console.log(req.url)
   console.log(req.method)
   console.log(req.headers)
 
-  if(req.url === '/'){
+
+if (req.url === "/") {
+
+    // res.setHeader('Content-Type','json')
     res.setHeader('Content-Type', 'text/html')
     res.write('<html>')
     res.write('<head><title>Home Page</title></head>')
@@ -19,26 +23,23 @@ const server = http.createServer((req, res) => {
     res.write('<br /><input type="submit" value="Submit" >')
     res.write('</form>')
     res.write('</html>')
-    return res.end()
-  }
-
-
-
-if (req.url === "/") {
-    // res.setHeader('Content-Type','json')
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>Home Page</title></head>");
-    res.write("<body><h1>Hello, I am Tanvir</h1></body>");
-    res.write("</html>");
     return res.end();
+
   } else if (req.url === "/products") {
+
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
     res.write("<head><title>Products Page</title></head>");
     res.write("<body><h1>Hello, This is Product Page</h1></body>");
     res.write("</html>");
     return res.end();
+
+  } else if(req.url.toLowerCase() === '/submit-details' && req.method === 'POST'){
+
+    fs.writeFileSync('user.txt','Tanvir Ahmed')
+    res.statusCode = 302 // 302 mane holo redirection
+    res.setHeader('Location','/')
+
   }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
