60a285d093cbbc614660cb81995455b332d303be
tanvirtbf <tanver018765@gmail.com>
2024-09-30 15:42:52 +0600

    parsing request start

--- Course/Section 2/test.sh
+++ Course/Section 2/test.sh
@@ -24,3 +24,42 @@
 #ls -la command 
 # ai command diye file folder gula details soho show hoy 
 
+#touch command
+# ai command diye only file create kora jay. folder create kora jay na
+#file create korar jonno touch command use kora hoy 
+# touch index.html (index.html file crated)
+# eksathe multilple file o create kora jay 
+# touch style.css index.js hello.html (all file created)
+
+
+#mkdir
+# ai command diye folder create kora hoy 
+# multiple folder o eksathe create kora jay 
+
+#cp command 
+# ai command diye jekono file onno kothau copy akare move kora jay 
+# syntext : cp moving_file [folder_name or path]
+
+#mv command 
+#ai command cp er motoi, kintu cp te copy hoye move hoito. ar mv diye cut hoye move hoy.
+# syntext : mv moving_file [folder_name or path]
+
+#mv command diye rename o kora jay 
+# syntext : cp real_file rename
+
+
+#rm command 
+# ai command diye jekono file remove kora hoy . folder remove kora jay na
+
+#rmdir command
+# ai command diye empty folder delete kora hoy
+
+#rm -r command 
+# ai command diye jekono folder jar vitor nested theke nested file folder ase airokom folder delete korar jonno use hoy
+
+
+# eksathe 1 hajar file create kora : 
+# for i in {1..1000}; do touch "app$i.js"; done
+
+# delete all 1000 file
+# for i in {1..1000}; do rm "app$i.js"; done
---
+++ Node 2/Parsing Request/app.js
@@ -0,0 +1,56 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  console.log(req.url)
+  console.log(req.method)
+  console.log(req.headers)
+
+
+if (req.url === "/") {
+
+    // res.setHeader('Content-Type','json')
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<html>')
+    res.write('<head><title>Home Page</title></head>')
+    res.write('<body><h1>Enter Your Details</h1></body>')
+    res.write('<form action="/submit-details" method="POST">')
+    res.write('<input type="text" name="username" placeholder="Enter your name"><br />')
+    res.write('<label for="male">Male</label>')
+    res.write('<input type="radio" id="male" name="gender" value="male" >')
+    res.write('<label for="female">Female</label>')
+    res.write('<input type="radio" id="female" name="gender" value="female" >')
+    res.write('<br /><input type="submit" value="Submit" >')
+    res.write('</form>')
+    res.write('</html>')
+    return res.end();
+
+  } else if (req.url === "/products") {
+
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Products Page</title></head>");
+    res.write("<body><h1>Hello, This is Product Page</h1></body>");
+    res.write("</html>");
+    return res.end();
+
+  } else if(req.url.toLowerCase() === '/submit-details' && req.method === 'POST'){
+
+    fs.writeFileSync('user.txt','Tanvir Ahmed')
+    res.statusCode = 302 // 302 mane holo redirection
+    res.setHeader('Location','/')
+
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>Error Page</title></head>");
+  res.write("<body><h1>404 Not Found</h1></body>");
+  res.write("</html>");
+  res.end();
+});
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on address http://localhost:${PORT}`);
+});
