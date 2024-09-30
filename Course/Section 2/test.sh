ac0593f83544be19a9b3ec27f2e85538aec8548b
tanvirtbf <tanver018765@gmail.com>
2024-09-30 11:19:10 +0600

    ls command complete

--- Course/Section 2/test.sh
+++ Course/Section 2/test.sh
@@ -11,5 +11,16 @@
 # ai command diye pc te user er name dekha jay 
 
 #cd command
+#cd means change directory
 #cd ../ (eta dile 1 folder pisone jay)
 #cd 'folder_name' eta dile oi folder e dhuke
+
+#ls command
+# ekta specific directoryr moddhe joto file folder ase sob gula show hobe ls command use korle 
+
+#ls -a command
+# ai command diye sob hidden file soho show hoy
+
+#ls -la command 
+# ai command diye file folder gula details soho show hoy 
+
