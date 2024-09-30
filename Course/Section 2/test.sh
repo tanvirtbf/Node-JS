a186fc4d31de75ab04b4864c3f1ae8938fc76c5e
tanvirtbf <tanver018765@gmail.com>
2024-09-30 11:10:10 +0600

    bash command

---
+++ Course/Section 2/test.sh
@@ -0,0 +1,15 @@
+#Variable Declare (echo)
+# num1 = 4
+# num2 = 5
+
+# echo $((num1+num2))
+
+#pwd command
+#ai command diye folder path dekha jay
+
+#whoami command
+# ai command diye pc te user er name dekha jay 
+
+#cd command
+#cd ../ (eta dile 1 folder pisone jay)
+#cd 'folder_name' eta dile oi folder e dhuke
