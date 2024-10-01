a9f1593d22e35ae1588190fb910ddc03c60f5ba2
tanvirtbf <tanver018765@gmail.com>
2024-10-01 09:46:06 +0600

    module export

---
+++ Node 2/Request & Response/index.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const userRequestHandler = require('./app')
+
+const server = http.createServer();
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on address http://localhost:${PORT}`);
+});
+
+
