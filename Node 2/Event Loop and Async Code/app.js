7f529f8b4fcc81e4b364e978fd55d3b1c57f655d
tanvirtbf <tanver018765@gmail.com>
2024-10-01 16:38:12 +0600

    all page routing complete

--- Node 2/Event Loop and Async Code/app.js
+++ Node 2/Event Loop and Async Code/app.js
@@ -14,6 +14,29 @@ const requestServer = (req,res) => {
     </html>
     `)
     res.end()
+  } else if(req.url.toLowerCase() === '/calculator'){
+    res.setHeader('Content-Type','text/html')
+    res.write(`
+    <html>
+      <head>
+        <title>Home Page</title>
+      </head>
+      <body>
+        <h1>Calculator</h1>
+        <form action="/calculate-result" method="POST">
+          <input type="text" name="first" placeholder="first number" >
+          <input type="text" name="second" placeholder="first number" >
+          <input type="submit" value="ADD">
+        </form>
+      </body>
+    </html>
+    `)
+    res.end()
+  } else if(req.url.toLowerCase() === '/calculate-result' && req.method === 'POST'){
+    res.statusCode = 302
+    res.setHeader('Location','/')
+    console.log('hiii')
+    res.end()
   }
 }
 
