c3dfaaa3e4e7b5dbac6a9bdab9f758c44e70d1c0
tanvirtbf <tanver018765@gmail.com>
2024-10-01 11:46:16 +0600

    project complete by me

--- Node 2/Request & Response/Calculator Project/app.js
+++ Node 2/Request & Response/Calculator Project/app.js
@@ -1,6 +1,10 @@
+const fs = require('fs')
+
 const requestServer = (req, res) => {
 
   console.log(req.url);
+
+
   const homePage = `
     <html>
       <head>
@@ -18,20 +22,40 @@ const requestServer = (req, res) => {
         <title>Document</title>
       </head>
       <body>
-        <form action="">
-          <input type="number" placeholder="type a number">
-          <input type="number" placeholder="type a number">
+        <form action="/calculator-data" method="POST">
+          <input type="number" name="firstNumber" placeholder="type a number">
+          <input type="number" name="lastNumber" placeholder="type a number">
           <button>Sum</button>
         </form>
       </body>
     </html>
     `;
 
+    let result ;
+
     if(req.url === '/'){
       res.write(homePage)
       res.end()
     }else if(req.url === '/calculator'){
       res.write(form)
+      res.end()
+    }else if(req.url === '/calculator-data' && req.method === 'POST'){
+      const body = []
+      req.on('data',(chunk)=>{
+        body.push(chunk)
+      })
+      req.on('end',()=>{
+        const formObject = {}
+        const fullBody = Buffer.concat(body).toString()
+        const bodyObject = new URLSearchParams(fullBody)
+        for(const [key,value] of bodyObject.entries()){
+          formObject[key] = value
+        }
+        result = Number(formObject.firstNumber) + Number(formObject.lastNumber) 
+        console.log(result)
+        fs.writeFileSync('user.txt', JSON.stringify(formObject))
+        res.write(`<h1>${result}</h1>`)
+      })
       res.end()
     }
 
