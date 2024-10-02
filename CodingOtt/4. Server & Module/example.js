6862e9844ab422409da2fa0134ada7e4d87e8edd
tanvirtbf <tanver018765@gmail.com>
2024-10-02 17:37:23 +0600

    module and exports complete

--- CodingOtt/4. Server & Module/example.js
+++ CodingOtt/4. Server & Module/example.js
@@ -1,3 +1,18 @@
-const name = "Tanvir Ahmed"
+// Exports single value
+// const name = "Tanvir Ahmed"
 
-module.exports = name
+// module.exports = name
+
+// Exports multiple value
+const a = 'Tanvir'
+const b = 'Ahmed'
+const c = 'Mustak'
+
+module.exports = {
+  a,
+  b,
+  c,
+}
+
+
+
