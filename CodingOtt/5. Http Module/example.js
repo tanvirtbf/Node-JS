e0450a7e564d920fc17500549785900540eaae3a
tanvirtbf <tanver018765@gmail.com>
2024-10-02 20:54:37 +0600

    module definition note

---
+++ CodingOtt/5. Http Module/example.js
@@ -0,0 +1,18 @@
+// Exports single value
+// const name = "Tanvir Ahmed"
+
+// module.exports = name
+
+// Exports multiple value
+const a = 'Tanvir'
+const b = 'Ahmed'
+const c = 'Mustak'
+
+module.exports = {
+  a,
+  b,
+  c,
+}
+
+
+
