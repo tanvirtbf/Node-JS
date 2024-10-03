2ce5346655183cc5cb1ecc7485db66c530c67377
tanvirtbf <tanver018765@gmail.com>
2024-10-03 09:53:39 +0600

    start response

---
+++ CodingOtt/6. Response/index.js
@@ -0,0 +1,12 @@
+// Modules
+
+
+// const name = require("./example");
+
+// console.log(name)
+
+const { a, b, c } = require("./example");
+
+console.log(a, b, c)
+
+
---
+++ CodingOtt/6. Response/note.js
@@ -0,0 +1,13 @@
+// what is module ? 
+// module mane holo reusable js file jeta onno kono js file er moddhe import export kore use kora hoy take module bole. jemon index.js holo main node js file . ecommerce site e cart, product, payment, category etc kajer jonno alada alada .js file create kore sekhane tader code kore index.js e tader import kora hoy jeno future e code maintain kora sohoj hoy . etao module . 
+
+// module 2 prokar 
+// 1. User Defined Module
+// 2. Built in Module or Predefined Module
+
+// 1. User Defined Module
+// eta holo ami nije jei js file create kore reuse kore import export kori seta 
+
+// 2. Built in Module or Predefined Module 
+// eta holo node js by default kisu kaj sohoj korar jonno kisu js file create kore export kore dise jate amra index.js file e import kore kaj korte pari . seta holo Build in Module . jemon : http, fs etc...
+
