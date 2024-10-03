506dbfad891c9be9d10a1d4e695f00091e70567e
tanvirtbf <tanver018765@gmail.com>
2024-10-03 12:32:42 +0600

    update

--- CodingOtt/8. Serving Static Files FS Module/index.js
+++ CodingOtt/8. Serving Static Files FS Module/index.js
@@ -1,10 +1,14 @@
 const http = require('http')
-
+const fs = require('fs')
 
 const server = http.createServer((req,res)=>{
 
+  fs.readFile('./ui/index.html', (error,data)=>{
+    res.writeHead(200, {'Content-Type':'text/html'})
+    res.write(data.toString())
+    res.end()
+  })
 
-  res.end()
 })
 
 server.listen(3000)
