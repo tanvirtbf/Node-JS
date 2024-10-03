77d223b652e4d60fe2e8414e040a5d7809fbc366
tanvirtbf <tanver018765@gmail.com>
2024-10-03 10:27:19 +0600

    update

--- CodingOtt/6. Response/index.js
+++ CodingOtt/6. Response/index.js
@@ -24,8 +24,13 @@ server.listen(3000)
 3. res.writeHead() 
 // eta diye response je pathaitesi server theke browser e seta ki type er data seta bole deya hoy browser ke . eta browser er header e save hoy
 // jodi response er data ki type er seta declare na kore dei tahole browser jodi nije theke bujhte pare eta ki dhoroner data tahole se sei datar response er header e Content-Type sei response datar type declare kore dey . kintu eta best way na . amader prottekta response er sathe Content-Type bole dite hobe 
+// Example:
+res.writeHead(200, {'Content-Type':'text/plain'})
 
 4. res.setHeader() 
 // eta same kaj kore res.writeHead() er motoii . just syntext ta alada 
+// Example : 
+res.setHeader('Content-Type','text/plain')
+
 
 */
