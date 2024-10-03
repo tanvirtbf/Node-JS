da623f8c73a370b2c622c0f624d6097aff9a300e
tanvirtbf <tanver018765@gmail.com>
2024-10-03 12:05:48 +0600

    update

--- CodingOtt/8. Serving Static Files FS Module/index.js
+++ CodingOtt/8. Serving Static Files FS Module/index.js
@@ -1,12 +1,8 @@
 const http = require('http')
-const url = require('url')
+
 
 const server = http.createServer((req,res)=>{
 
-  const parseUrl = url.parse(req.url, true)
-  
-  res.write(`<h1>Name : ${parseUrl.query.name}</h1>`)
-  res.write(`<h1>Age : ${parseUrl.query.age}</h1>`)
 
   res.end()
 })
