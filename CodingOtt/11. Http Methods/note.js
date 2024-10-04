4b8787acd764ff83b610482b2cae9c265a2acb30
tanvirtbf <tanver018765@gmail.com>
2024-10-04 18:03:12 +0600

    start a simple progject

---
+++ CodingOtt/11. Http Methods/note.js
@@ -0,0 +1,16 @@
+/*
+
+All file system function 
+
+1. fs.readFile --- ei function diye kono file read kora jabe
+
+2. fs.writeFile --- ei function diye kono new file create kore sekhane kichu likha jabe
+
+3. fs.appendFile --- ei function diye old file e jodi kichu content thake tahole setar sathe niye notun kichu likha jabe . tar mane old content+new content 
+
+4. fs.rename() --- file er name change korar jonno
+
+5. fs.unlink() --- file delete korar jonno use hoy
+
+
+*/
