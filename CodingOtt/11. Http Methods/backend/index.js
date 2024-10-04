4999ce47af53092d8f8d6fcc29f3613c024eb5ef
tanvirtbf <tanver018765@gmail.com>
2024-10-04 18:17:30 +0600

    connect frontend to backend

--- CodingOtt/11. Http Methods/backend/index.js
+++ CodingOtt/11. Http Methods/backend/index.js
@@ -1,6 +1,9 @@
 const http = require('http')
 
 const server = http.createServer((req,res)=>{
+    res.writeHead(200, {
+        'Access-Control-Allow-Origin': 'http://127.0.0.1:5500'
+    })
     res.write('server')
     res.end()
 })
