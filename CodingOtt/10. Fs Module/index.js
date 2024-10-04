dcfefd2ad363e582bc056729e21daf0bd5d2e011
tanvirtbf <tanver018765@gmail.com>
2024-10-04 17:05:34 +0600

    fs.readFile test complete

--- CodingOtt/10. Fs Module/index.js
+++ CodingOtt/10. Fs Module/index.js
@@ -1,1 +1,19 @@
 const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req,res)=>{
+
+  // fs.readFile
+  // const data = fs.readFile('./static/hello.txt', (err,data)=>{
+  //   if(err){
+  //     res.write('File Error!')
+  //     return res.end()
+  //   }else {
+  //     res.write(data)
+  //     return res.end()
+  //   }
+  // })
+
+})
+
+server.listen(3000)
