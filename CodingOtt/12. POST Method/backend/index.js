fb9f7fde2cf1b1908aed54a76c5627bb16ecd474
tanvirtbf <tanver018765@gmail.com>
2024-10-04 22:54:05 +0600

    update

--- CodingOtt/12. POST Method/backend/index.js
+++ CodingOtt/12. POST Method/backend/index.js
@@ -6,7 +6,11 @@ const server = http.createServer((req,res)=>{
     res.writeHead(200, {
         'Access-Control-Allow-Origin' : 'http://127.0.0.1:5500'
     })
-    res.write('hello')
+    const data = JSON.stringify({
+        username:'Tanvir',
+        password:123
+    })
+    res.write(data)
     res.end()
 
 })
