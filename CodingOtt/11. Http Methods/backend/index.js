beacfbcd40a5643b1eaa1832ecafd61f41c597b4
tanvirtbf <tanver018765@gmail.com>
2024-10-04 21:44:02 +0600

    pass query string

--- CodingOtt/11. Http Methods/backend/index.js
+++ CodingOtt/11. Http Methods/backend/index.js
@@ -4,6 +4,7 @@ const server = http.createServer((req,res)=>{
     res.writeHead(200, {
         'Access-Control-Allow-Origin': 'http://127.0.0.1:5500'
     })
+    console.log(req.url)
     const str = JSON.stringify({name:'Tanvir', age:25})
     res.write(str)
     res.end()
