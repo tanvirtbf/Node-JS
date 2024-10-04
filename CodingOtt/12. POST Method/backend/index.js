a1b96977a59065a912837d7ca9e0d843dad002a7
tanvirtbf <tanver018765@gmail.com>
2024-10-04 22:33:54 +0600

    connect frontend to backend

--- CodingOtt/12. POST Method/backend/index.js
+++ CodingOtt/12. POST Method/backend/index.js
@@ -2,15 +2,13 @@ const http = require('http')
 const url = require('url')
 
 const server = http.createServer((req,res)=>{
+
     res.writeHead(200, {
-        'Access-Control-Allow-Origin': 'http://127.0.0.1:5500'
+        'Access-Control-Allow-Origin' : 'http://127.0.0.1:5500'
     })
-    const parseUrl = url.parse(req.url, true)
-    const username = parseUrl.query.username 
-    const password = parseUrl.query.password 
-    const str = JSON.stringify({username:username, password:password})
-    res.write(str)
+    res.write('hello')
     res.end()
+
 })
 
 server.listen(3000)
