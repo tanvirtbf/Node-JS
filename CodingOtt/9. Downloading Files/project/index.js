1a1a824e68dbe3877815504133315f9f7e5675ff
tanvirtbf <tanver018765@gmail.com>
2024-10-12 09:31:08 +0600

    update

--- CodingOtt/9. Downloading Files/project/index.js
+++ CodingOtt/9. Downloading Files/project/index.js
@@ -1,3 +1,30 @@
 const http = require('http')
 const fs = require('fs')
 const url = require('url')
+
+const server = http.createServer((req,res)=>{
+
+  const parseUrl = url.parse(req.url, true)
+  const queryStr = parseUrl.query.filename 
+  const path = parseUrl.pathname 
+
+  if(queryStr === 'hello1.pdf'){
+    const data = fs.readFileSync(`/${queryStr}`)
+    res.writeHead(200, {
+      'Content-Type':'application/pdf',
+      'Content-Disposition", "attachment; filename="" "'
+    })
+  }
+
+  res.end()
+})
+
+server.listen(3000)
+
+
+
+
+
+
+
+
