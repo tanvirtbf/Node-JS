9b0c1a2f046816b617e32c73eb28136f4dbeea5a
tanvirtbf <tanver018765@gmail.com>
2024-10-12 12:51:48 +0600

    Rest api complete

--- P Node/Build Rest API with Express/index.js
+++ P Node/Build Rest API with Express/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const fs = require('fs')
 const users = require('./users.json')
 
 const app = express()
+
+// Middleware (for accept body)
+app.use(express.urlencoded({ extended: false}))
 
 // REST API (GET)
 app.get('/api/users', (req,res) => {
@@ -26,8 +30,13 @@ app.get('/api/users/:id', (req,res)=>{
 // POST REST API 
 app.post('/api/users' ,(req,res)=>{
   // Create New User
-  res.json({status:'pending'})
+  const body = req.body
+  users.push({...body, id: users.length + 1})
+  fs.writeFile('./users.json', JSON.stringify(users), (err,data)=>{
+    return res.json({status:'success', id: users.length + 1})
+  })
 })
+
 
 // PATCH REST API 
 app.post('/api/users/:id' ,(req,res)=>{
