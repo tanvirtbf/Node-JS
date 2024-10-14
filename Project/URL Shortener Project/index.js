c4b4077933534397920f72f3a42eb0a120fdbd34
tanvirtbf <tanver018765@gmail.com>
2024-10-14 13:06:35 +0600

    ejs use

--- Project/URL Shortener Project/index.js
+++ Project/URL Shortener Project/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const urlRoute = require('./routes/urlRoutes.js')
 const { connectDb } = require('./connection.js')
 const URL = require('./models/url.js')
+const path = require('path')
 
 
 const app = express()
@@ -10,22 +11,22 @@ connectDb('mongodb://127.0.0.1:27017/urlShortner')
   .then(()=> console.log('MongoDB Connected!'))
   .catch((err)=> console.log(err))
 
+// express ke bole dilam ami kon template engine use korchi..
+app.set('view engine', 'ejs')
+
+// amar ejs file ami kothay rekhechi seta bole dite hobe..
+app.set('views', path.resolve('./views'))
+
 // Middleware (for accept form data as json)
 app.use(express.json())
 
 // Routes
 app.get('/test', async (req,res)=>{
   const allUrls = await URL.find({})
-  return res.end(`
-    <html>
-      <head></head>
-      <body>
-        <ol>
-          ${allUrls.map(url => `<li>${url.shortId} - ${url.redirectUrl} - ${url.visitHistory.length}</li>`).join("")}
-        </ol>
-      </body>
-    </html>  
-  `)
+  return res.render('home', {
+    urls : allUrls,
+    name : 'Tanvir'
+  })
 })
 app.use('/url', urlRoute)
 
