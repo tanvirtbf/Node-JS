1855f7ae3142bd23015ba4233b766b95a60afb9b
tanvirtbf <tanver018765@gmail.com>
2025-04-08 11:38:55 +0600

    path module complete

--- NodeJS Complete/Path Module/path_module.js
+++ NodeJS Complete/Path Module/path_module.js
@@ -1,4 +1,20 @@
 const path = require('path');
 
-console.log(__dirname);
-console.log(__filename)
+// console.log(__dirname);
+// console.log(__filename);
+
+const filePath = path.join("folder", "student", "data.txt")
+console.log(filePath)
+
+const parseData = path.parse(filePath)
+const resolvedPath = path.resolve(filePath)
+const extname = path.extname(filePath)
+const basename = path.basename(filePath)
+const dirname = path.dirname(filePath)
+console.log({
+    parseData,
+    resolvedPath,
+    extname,
+    basename,
+    dirname
+})
