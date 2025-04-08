e11ce8c1dd555e07629e6c2485dcd7ec011eccc8
tanvirtbf <tanver018765@gmail.com>
2025-04-08 14:49:44 +0600

    writeFile Async

--- NodeJS Complete/FS Module/asyncfs.js
+++ NodeJS Complete/FS Module/asyncfs.js
@@ -1,5 +1,16 @@
 const fs = require('fs')
 const path = require('path')
 
-const filename = 'test.txt'
+const filename = 'asyncTest.txt'
 const filePath = path.join(__dirname, filename)
+
+
+const writeFile = fs.writeFile(filePath, 'Hello World!', (err) => {
+    if (err) {
+        console.error(err)
+        return
+    }else{
+        console.log('File written successfully')
+    }
+})
+
