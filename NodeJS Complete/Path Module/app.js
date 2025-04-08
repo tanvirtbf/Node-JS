4a68b03f150f262e7b2af5ce8e8a0eea2c6ffd25
tanvirtbf <tanver018765@gmail.com>
2025-04-08 11:30:42 +0600

    __dirname and __filename explore

---
+++ NodeJS Complete/Path Module/app.js
@@ -0,0 +1,10 @@
+const {add,subs,mult} = require('./math.js')
+// const add = math.add
+// const subs = math.subs
+// const mult = math.mult
+
+console.log(add(5,10))
+console.log(subs(5,10))
+console.log(mult(5,10))
+
+
---
+++ NodeJS Complete/Path Module/math.js
@@ -0,0 +1,19 @@
+const add = (a,b) => {
+    return a+b;
+}
+const subs = (a,b) => {
+    return a-b;
+}
+const mult = (a,b) => {
+    return a*b;
+}
+
+module.exports.add = add;
+module.exports.subs = subs;
+module.exports.mult = mult;
+
+// module.exports = {
+//     add,
+//     subs,
+//     mult
+// }
