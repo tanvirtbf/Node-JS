bb47f0f1e8cc68212544b58748ce0e9e81c39f2f
tanvirtbf <tanver018765@gmail.com>
2025-04-08 17:56:24 +0600

    Server created

---
+++ NodeJS Complete/Web Server/server.js
@@ -0,0 +1,21 @@
+const { log } = require('console');
+const http = require('http');
+
+const server = http.createServer((req, res) =>{
+    if(req.url === '/'){
+        res.write('<h1>I am Tanvir Ahmed</h1>') 
+        res.end()
+    }
+    if(req.url === '/contact'){
+        res.setHeader('Content-Type', 'text/html')
+        res.write('This is the contact page')
+        res.end()
+    }
+})
+
+// Web Server 
+
+const port = 3000
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
