47456395a8683717fdf5fc0574abda51fa454bce
tanvirtbf <tanver018765@gmail.com>
2025-04-09 17:00:58 +0600

    now own

--- NodeJS Complete/Mini Project/URL Shortner App/app.js
+++ NodeJS Complete/Mini Project/URL Shortner App/app.js
@@ -1,32 +1,64 @@
-import { readFile } from 'fs/promises';
-import {createServer} from 'http';
-import path from 'path';
 
-const PORT = 3000;
 
-const serveFile = async (res, filePath, contentType) => {
-    try {
-        const data = await readFile(filePath)
-        res.writeHead(200, {'Content-Type': contentType});
-        res.end(data)
-    } catch (error) {
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        res.end('404 Page Not Found!');
-    }
-}
 
-const server = createServer( async (req, res)=>{
-    console.log(req.url)
-    if(req.method === "GET"){
-        if(req.url === "/"){
-            return serveFile(res, path.join('public','index.html'), 'text/html')
-        }else if(req.url === "/style.css"){
-            return serveFile(res, path.join('public','style.css'), 'text/css')
-        }
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { readFile } from 'fs/promises';
+// import {createServer} from 'http';
+// import path from 'path';
+
+// const PORT = 3000;
+
+// const serveFile = async (res, filePath, contentType) => {
+//     try {
+//         const data = await readFile(filePath)
+//         res.writeHead(200, {'Content-Type': contentType});
+//         res.end(data)
+//     } catch (error) {
+//         res.writeHead(404, {'Content-Type': 'text/html'});
+//         res.end('404 Page Not Found!');
+//     }
+// }
+
+// const server = createServer( async (req, res)=>{
+//     console.log(req.url)
+//     if(req.method === "GET"){
+//         if(req.url === "/"){
+//             return serveFile(res, path.join('public','index.html'), 'text/html')
+//         }else if(req.url === "/style.css"){
+//             return serveFile(res, path.join('public','style.css'), 'text/css')
+//         }
+//     }
         
-})
+// })
 
-server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-})
+// server.listen(PORT, () => {
+//     console.log(`Server is running on http://localhost:${PORT}`);
+// })
