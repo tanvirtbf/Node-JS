243d41bc13973c5f240de1a4b7b2438817b0b89f
tanvirtbf <tanver018765@gmail.com>
2025-04-09 17:08:54 +0600

    own project practice complete part 2

--- NodeJS Complete/Mini Project/URL Shortner App/app.js
+++ NodeJS Complete/Mini Project/URL Shortner App/app.js
@@ -1,8 +1,35 @@
+import path from 'path';
+import { readFile } from 'fs/promises';
+import { createServer } from 'http';
 
 
+const server = createServer(async (req, res) => {
+    if(req.method==='GET'){
+        if(req.url ==='/'){
+            try {
+                const data = await readFile(path.join('public', 'index.html'))
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end(data)
+            } catch (error) {
+                res.writeHead(404, {'Content-Type': 'text/html'});
+                res.end('404 Page not found!')
+            }
+        }else if(req.url ==='/style.css'){
+            try {
+                const data = await readFile(path.join('public', 'style.css'))
+                res.writeHead(200, {'Content-Type': 'text/css'});
+                res.end(data)
+            } catch (error) {
+                res.writeHead(404, {'Content-Type': 'text/html'});
+                res.end('404 Page not found!')
+            }
+        }
+    }
+});
 
-
-
+server.listen(3000, () =>{
+    console.log('Server is running on http://localhost:3000')
+})
 
 
 
