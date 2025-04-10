e382a51b011784ef1c72f4d6034d7afb8bcd25a1
tanvirtbf <tanver018765@gmail.com>
2025-04-10 17:48:16 +0600

    manual validate PORT

--- NodeJS Complete/Express/app.js
+++ NodeJS Complete/Express/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { PORT } from "./env.js";
 
 const app = express();
 
@@ -101,7 +102,7 @@ app.get('/contact', (req, res) => {
       `);
 })
 
-const PORT = process.env.PORT || 3000
+// const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:3000`)
 })
---
+++ NodeJS Complete/Express/env.js
@@ -0,0 +1,2 @@
+export const PORT = isNaN(process.env.PORT) ? 3000 : parseInt(process.env.PORT); 
+
