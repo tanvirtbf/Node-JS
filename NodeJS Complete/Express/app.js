f50e09b3ecc240312aff65f0086cb4a8bc0d3f62
tanvirtbf <tanver018765@gmail.com>
2025-04-10 16:24:15 +0600

    working with env

--- NodeJS Complete/Express/app.js
+++ NodeJS Complete/Express/app.js
@@ -101,6 +101,7 @@ app.get('/contact', (req, res) => {
       `);
 })
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:3000`)
 })
