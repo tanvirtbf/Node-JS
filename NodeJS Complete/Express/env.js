3f85fb01b72bdb9d11ef9aaefc44c43145dfdd9e
tanvirtbf <tanver018765@gmail.com>
2025-04-12 17:58:05 +0600

    update

--- NodeJS Complete/Express/env.js
+++ NodeJS Complete/Express/env.js
@@ -1,8 +1,19 @@
 export const PORT = isNaN(process.env.PORT) ? 3000 : parseInt(process.env.PORT); 
 
-import {z} from 'zod';
+import {z, ZodError} from 'zod';
 
 const ageSchema = z.number().min(18).max(100).int().positive();
-const userAge = 19;
-const parseUserAge = ageSchema.parse(userAge);
-console.log(parseUserAge);
+const userAge = 17;
+// const parseUserAge = ageSchema.parse(userAge);
+// console.log(parseUserAge);
+
+try {
+  const parsedUserAge = ageSchema.parse(userAge)
+  console.log(parsedUserAge)
+} catch (error) {
+  if(error instanceof ZodError){ // er ortho hocche jei error amra pacchi seta ki ZodError er ? 
+    console.log(error.issues[0].message)
+  }else {
+    console.log("Unexpected error: ", error)
+  }
+}
