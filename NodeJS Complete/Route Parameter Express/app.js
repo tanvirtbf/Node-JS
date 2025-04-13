68707f4ea3e05767f62c1afeb2905efcc9d77b5f
tanvirtbf <tanver018765@gmail.com>
2025-04-13 16:54:42 +0600

    error page controlled

--- NodeJS Complete/Route Parameter Express/app.js
+++ NodeJS Complete/Route Parameter Express/app.js
@@ -4,15 +4,26 @@ import path from "path";
 
 const app = express();
 
+app.use(express.urlencoded({extended: true}))
+
 const absolutePath = path.join(import.meta.dirname, 'public')
 app.use(express.static(absolutePath))
 
 // app.get('/contact', (req, res) => {
 //     console.log(req.query);
-//     res.send('Hi')
-//     // res.redirect('/')
+//     // res.send('Hi')
+//     res.redirect('/')
 // })
 
+app.post('/contact', (req, res) => {
+    console.log(req.body);
+    res.redirect('/')
+})
+
+app.use((req, res) => {
+    // res.status(404).send('Page Not Found!')
+    res.status(404).sendFile(path.join(import.meta.dirname, "public", "404.html"))
+})
 
 // Practice Route Parameter
 // app.get('/profile/:username', (req, res) => {
