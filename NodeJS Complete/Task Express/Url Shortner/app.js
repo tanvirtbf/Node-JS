eff25514d3c066475f9dae6ab792a63d995e7f1e
tanvirtbf <tanver018765@gmail.com>
2025-04-14 11:39:31 +0600

    update

--- NodeJS Complete/Task Express/Url Shortner/app.js
+++ NodeJS Complete/Task Express/Url Shortner/app.js
@@ -1,11 +1,22 @@
-import express from 'express'
+import express, { urlencoded } from 'express'
+import path from 'path'
 
 const app = express()
+
+const absolutePath = path.join(import.meta.dirname, "public")
+app.use(express.static(absolutePath))
+
+app.use(urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
     res.send('Hii')
 })
 
+app.post('/shorten', (req, res) => {
+    const h = req.body
+    console.log(h);
+    res.status(200).send('Hi')
+})
 
 app.listen(3000, () => {
     console.log(`Running port on http://localhost:3000`);
