63f6fd603e589622dcb92385025817af32d30399
tanvirtbf <tanver018765@gmail.com>
2025-04-15 15:38:05 +0600

    complete

--- NodeJS Complete/Task Express/Url Shortner/app.js
+++ NodeJS Complete/Task Express/Url Shortner/app.js
@@ -1,15 +1,89 @@
 import express from 'express'
+import { readFile } from 'fs/promises';
+import { writeFile } from 'fs/promises';
+import fs from 'fs/promises'
 import path from 'path'
 
 const app = express()
 
-const filePath = path.join(import.meta.dirname, 'public', 'style.css')
+const PORT = process.env.PORT || 3000;
+const DATA_FILE = path.join("data", "links.json");
 
-app.use(express.static(filePath))
+// app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+
+const loadLinks = async () => {
+  try {
+    const data = await readFile(DATA_FILE, "utf-8");
+    return JSON.parse(data);
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      await writeFile(DATA_FILE, JSON.stringify({}));
+      return {};
+    }
+    throw error;
+  }
+};
+
+const saveLinks = async (links) => {
+  await writeFile(DATA_FILE, JSON.stringify(links));
+};
+
+app.get("/", async (req, res) => {
+  try {
+    const file = await fs.readFile(path.join("views", "index.html"));
+    const links = await loadLinks();
+
+    const content = file.toString().replaceAll(
+      "{{ shortened-urls }}",
+      Object.entries(links)
+        .map(
+          ([shortCode, url]) =>
+            `<li><a href="/${shortCode}" target="_blank">${req.host}/${shortCode}</a> -> ${url} </li>`
+        )
+        .join("")
+    );
+    return res.send(content)
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Internal Server Error!");
+  }
+});
+
+app.get("/:shortCode", async (req, res) => {
+  try {
+    const {shortCode} = req.params
+    const links = await loadLinks();
+
+    if(!links[shortCode]) return res.status(404).send('404 error occured')
+
+    return res.redirect(links[shortCode])
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Internal Server Error!");
+  }
+});
 
 
-app.listen(3000, () => {
-  console.log(`PORT Running in http://localhost:3000/`);
+app.post('/', async (req, res) => {
+  try {
+    const { url, shortCode } = req.body;
+    const links = loadLinks()
+    const finalShortCode = shortCode || crypto.randomBytes(4).toString("hex");
+    if(links[finalShortCode]){
+      return res.send('ShortCode are already Exists!')
+    }
+    links[finalShortCode] = url
+    await saveLinks(links)
+    return res.redirect('/')
+  } catch (error) {
+    res.status(404).send('Form Not Saved Successfully!')
+  }
+})
+
+
+app.listen(PORT, () => {
+  console.log(`PORT Running in http://localhost:${PORT}/`);
 })
 
 
