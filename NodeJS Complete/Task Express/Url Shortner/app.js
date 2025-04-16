3f12960dbfba5ac80ec01c9a75b1596a6220f1df
tanvirtbf <tanver018765@gmail.com>
2025-04-16 09:41:26 +0600

    ejs start

--- NodeJS Complete/Task Express/Url Shortner/app.js
+++ NodeJS Complete/Task Express/Url Shortner/app.js
@@ -8,6 +8,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
+app.set('view engine', 'ejs');
+
 // express router
 // app.use(router)
 app.use(shortenerRoutes);
