4694f6a4dbdff5ecec8db6d30ff63e312b01215e
tanvirtbf <tanver018765@gmail.com>
2025-04-17 17:28:28 +0600

    hole project shift in ejs

--- NodeJS Complete/Task Express/Url Shortner/controller/postshortener.controller.js
+++ NodeJS Complete/Task Express/Url Shortner/controller/postshortener.controller.js
@@ -1,5 +1,7 @@
 import crypto from "crypto";
-
+import path from "path";
+import { loadLinks, saveLinks } from "../model/shortener.model.js";
+import { readFile } from "fs/promises";
 
 export const postURLShortener = async (req, res) => {
   try {
@@ -20,6 +22,31 @@ export const postURLShortener = async (req, res) => {
     return res.redirect("/");
   } catch (error) {
     console.error(error);
+    return res.status(500).send("Internal server error");
+  }
+};
+
+export const getShortenerPage = async (req, res) => {
+  try {
+    // const file = await readFile(path.join("views", "index.html"));
+    const links = await loadLinks();
+    return res.render("index", { links, host: req.host });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Internal server error");
+  }
+};
+
+export const redirectToShortLink = async (req, res) => {
+  try {
+    const { shortCode } = req.params;
+    const links = await loadLinks();
+
+    if (!links[shortCode]) return res.status(404).send("404 error occurred");
+
+    return res.redirect(links[shortCode]);
+  } catch (err) {
+    console.error(err);
     return res.status(500).send("Internal server error");
   }
 };
