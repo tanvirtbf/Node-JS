17bc9e25fb16b58d6c7b0470c5bc7a82dcc6d49a
tanvirtbf <tanver018765@gmail.com>
2025-04-21 22:14:58 +0600

    use middleware test

--- Express 1/Basic Express/app.js
+++ Express 1/Basic Express/app.js
@@ -3,7 +3,26 @@ const express = require('express');
 
 const app = express();
 
-
+app.use('/', (req, res, next) => {
+  console.log("Came in first middleware", req.url, req.method);
+  next();
+})
+app.use('/submit-details', (req, res, next) => {
+  console.log("Came in Hello World! middleware", req.url, req.method);
+  next();
+})
+app.use('/submit-details/hello', (req, res, next) => {
+  console.log("Came in second middleware", req.url, req.method);
+  next();
+})
+app.use('/submit-details/hello', (req, res, next) => {
+  console.log("Came in third middleware", req.url, req.method);
+  next();
+})
+app.use('/submit-details/hello', (req, res, next) => {
+  console.log("Came in second middleware", req.url, req.method);
+  res.send("<p>Welcome to Complete Coding Nodejs series</p>");
+})
 
 // app.get("/", (req, res, next) => {
 //   console.log("Came in first middleware", req.url, req.method);
@@ -21,7 +40,7 @@ const app = express();
 //   res.send("<p>Came from another Middleware</p>");
 // });
 
-const PORT = 3002;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on address http://localhost:${PORT}`);
 });
