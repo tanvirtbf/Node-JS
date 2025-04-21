011afb868b018bf7b638578e8004d93c49a3757e
tanvirtbf <tanver018765@gmail.com>
2025-04-21 23:48:43 +0600

    updat4e

--- Express 1/Express airbnb Project/app.js
+++ Express 1/Express airbnb Project/app.js
@@ -4,6 +4,14 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const hostRouter = require('./routes/host');
+const userRouter = require('./routes/user');
+
+app.use((req, res, next) => {
+  console.log(req.url, req.method);
+  res.send("<h1>Welcome to AirBnb Server</h1>");
+});
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
