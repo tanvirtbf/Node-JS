4eb696308d73f3cc9a829d0e4ce00563ba8eac4f
tanvirtbf <tanver018765@gmail.com>
2025-04-21 22:08:26 +0600

    start some practice

--- Express 1/Basic Express/app.js
+++ Express 1/Basic Express/app.js
@@ -3,21 +3,23 @@ const express = require('express');
 
 const app = express();
 
-app.get("/", (req, res, next) => {
-  console.log("Came in first middleware", req.url, req.method);
-  //res.send("<p>Came from First Middleware</p>");
-  next();
-});
 
-app.post("/submit-details", (req, res, next) => {
-  console.log("Came in second middleware", req.url, req.method);
-  res.send("<p>Welcome to Complete Coding Nodejs series</p>");
-});
 
-app.use("/", (req, res, next) => {
-  console.log("Came in another middleware", req.url, req.method);
-  res.send("<p>Came from another Middleware</p>");
-});
+// app.get("/", (req, res, next) => {
+//   console.log("Came in first middleware", req.url, req.method);
+//   //res.send("<p>Came from First Middleware</p>");
+//   next();
+// });
+
+// app.post("/submit-details", (req, res, next) => {
+//   console.log("Came in second middleware", req.url, req.method);
+//   res.send("<p>Welcome to Complete Coding Nodejs series</p>");
+// });
+
+// app.use("/", (req, res, next) => {
+//   console.log("Came in another middleware", req.url, req.method);
+//   res.send("<p>Came from another Middleware</p>");
+// });
 
 const PORT = 3002;
 app.listen(PORT, () => {
