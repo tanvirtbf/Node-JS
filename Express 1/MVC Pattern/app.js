62a1abeef2ccf1d5de1514e0e2b91088019ef3ee
tanvirtbf <tanver018765@gmail.com>
2025-04-24 21:57:05 +0600

    models work start

--- Express 1/MVC Pattern/app.js
+++ Express 1/MVC Pattern/app.js
@@ -1,28 +1,28 @@
 // Core Module
-const path = require('path');
+const path = require("path");
 
 // External Module
-const express = require('express');
+const express = require("express");
 
 //Local Module
-const userRouter = require("./routes/userRouter")
-const {hostRouter} = require("./routes/hostRouter")
+const userRouter = require("./routes/userRouter");
+const { hostRouter } = require("./routes/hostRouter");
 const rootDir = require("./utils/pathUtil");
-const { errorController } = require('./controllers/404');
+const { errorController } = require("./controllers/404");
 
 const app = express();
 
-app.set('view engine', 'ejs'); 
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
 app.use(express.urlencoded());
 
 app.use(userRouter);
 app.use("/host", hostRouter);
 
-app.use(express.static(path.join(rootDir, 'public')))
+app.use(express.static(path.join(rootDir, "public")));
 
-app.use(errorController)
+app.use(errorController);
 
 const PORT = 3000;
 app.listen(PORT, () => {
