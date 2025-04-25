75e2036762271aa58c7ecfba77287a0d9469ac98
tanvirtbf <tanver018765@gmail.com>
2025-04-25 12:42:10 +0600

    update

--- Express 1/Practice Set/Practice Mvc Pattern Part/app.js
+++ Express 1/Practice Set/Practice Mvc Pattern Part/app.js
@@ -1,11 +1,26 @@
 import express from 'express'
+import homeRouter from './routes/homeRouter.js';
+import path from 'path';
+import { rootDir } from './utils/pathUtil.js';
 
 const app = express();
 
-app.set('views', 'ejs')
+
+app.use(express.urlencoded()) // body parse korar jonno ai middleware
+app.use(express.static(path.join(rootDir, 'public'))) // akhn public folder er sob file publicly access kora jabe 
+
+app.set('view engine', 'ejs')
 app.set('views', 'views')
 
+app.use(homeRouter)
+
+app.use((req, res, next) => {
+    res.send('404 page not found!')
+})
 
 app.listen(4000, ()=>{
     console.log(`Port Running in http://localhost:4000`);
 })
+
+
+
---
+++ Express 1/Practice Set/Practice Mvc Pattern Part/controllers/userController.js
@@ -0,0 +1,7 @@
+export const userGetController = (req, res) => {
+    res.send('User Form Page')
+}
+
+export const userPostController = (req, res) => {
+    res.send('User Post Form')
+}
