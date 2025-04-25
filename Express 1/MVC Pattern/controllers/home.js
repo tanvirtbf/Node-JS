c58f05a1369f93e43890bd3a09822d8ef38e5bf9
tanvirtbf <tanver018765@gmail.com>
2025-04-25 12:03:39 +0600

    home save some data

--- Express 1/MVC Pattern/controllers/home.js
+++ Express 1/MVC Pattern/controllers/home.js
@@ -1,3 +1,5 @@
+const Home = require("../models/home");
+
 exports.getAddHome = (req, res, next) => {
   res.render("addHome", {
     pageTitle: "Add Home to airbnb",
@@ -9,6 +11,10 @@ exports.getAddHome = (req, res, next) => {
 
 exports.postAddHome = (req, res, next) => {
   console.log("Home Registration successful for:", req.body);
+
+  const home = new Home(req.body.houseName, req.body.price, req.body.location, req.body.rating, req.body,photoUrl)
+  home.save();
+
   registeredHomes.push(req.body);
   res.render("homeAdded", {
     pageTitle: "Home Added Successfully",
