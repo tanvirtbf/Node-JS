e1a8042ba0794a24acf57f258abffeb09313ca77
tanvirtbf <tanver018765@gmail.com>
2025-04-25 22:52:43 +0600

    nodemon conflict complete

--- Express 1/MVC Pattern/controllers/home.js
+++ Express 1/MVC Pattern/controllers/home.js
@@ -1,6 +1,3 @@
-const fs = require('fs')
-const path = require('path')
-
 
 const Home = require("../models/home");
 const path = require('path');
--- Express 1/MVC Pattern/models/home.js
+++ Express 1/MVC Pattern/models/home.js
@@ -1,22 +1,28 @@
-// fake database 
+const fs = require("fs");
+const path = require("path");
+const rootDir = require("../utils/pathUtil");
+
+// fake database
 const registeredHomes = [];
 
 module.exports = class Home {
-    constructor(houseName, price, location, rating, photoUrl){
-        this.houseName = houseName;
-        this.price = price;
-        this.location = location;
-        this.rating = rating;
-        this.photoUrl = photoUrl;
-    }
+  constructor(houseName, price, location, rating, photoUrl) {
+    this.houseName = houseName;
+    this.price = price;
+    this.location = location;
+    this.rating = rating;
+    this.photoUrl = photoUrl;
+  }
 
-    save() {
-        registeredHomes.push(this)
-    }
+  save() {
+    registeredHomes.push(this);
+    const homeDataPath = path.join(rootDir, "data", "homes.json");
+    fs.writeFile(homeDataPath, JSON.stringify(registeredHomes), (error) => {
+      console.log("File Writting Concluded", error);
+    });
+  }
 
-    static fetchAll(){
-        return registeredHomes;
-    }
-
-}
-
+  static fetchAll() {
+    return registeredHomes;
+  }
+};
