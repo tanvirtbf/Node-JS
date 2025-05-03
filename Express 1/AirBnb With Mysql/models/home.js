ee85bf2cfc2963c6c8652459b1b4b49d651d3c72
tanvirtbf <tanver018765@gmail.com>
2025-05-03 11:20:06 +0600

    update

--- Express 1/AirBnb With Mysql/models/home.js
+++ Express 1/AirBnb With Mysql/models/home.js
@@ -2,12 +2,14 @@
 const db = require("../utils/databaseUtil");
 
 module.exports = class Home {
-  constructor(houseName, price, location, rating, photoUrl) {
+  constructor(houseName, price, location, rating, photoUrl, description, id) {
     this.houseName = houseName;
     this.price = price;
     this.location = location;
     this.rating = rating;
     this.photoUrl = photoUrl;
+    this.description = description;
+    this.id = id;
   }
 
   save() {}
