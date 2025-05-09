57b508dbb089fbabfc0f147e1cf05de552746589
tanvirtbf <tanver018765@gmail.com>
2025-05-09 16:00:27 +0600

    update

--- Express 1/AirBnb With MongoDB/controllers/hostController.js
+++ Express 1/AirBnb With MongoDB/controllers/hostController.js
@@ -10,9 +10,9 @@ exports.getAddHome = (req, res, next) => {
 
 exports.getEditHome = (req, res, next) => {
   const homeId = req.params.homeId;
-  const editing = req.query.editing === "true";
+  const editing = req.query.editing === 'true';
 
-  Home.findById(homeId).then((home) => {
+  Home.findById(homeId).then(home => {
     if (!home) {
       console.log("Home not found for editing.");
       return res.redirect("/host/host-home-list");
@@ -29,59 +29,40 @@ exports.getEditHome = (req, res, next) => {
 };
 
 exports.getHostHomes = (req, res, next) => {
-  Home.fetchAll().then((registeredHomes) => {
+  Home.fetchAll().then(registeredHomes => {
     res.render("host/host-home-list", {
       registeredHomes: registeredHomes,
       pageTitle: "Host Homes List",
       currentPage: "host-homes",
-    });
+    })
   });
 };
 
 exports.postAddHome = (req, res, next) => {
-  const { houseName, price, location, rating, photoUrl, description } =
-    req.body;
-  const home = new Home(
-    houseName,
-    price,
-    location,
-    rating,
-    photoUrl,
-    description
-  );
+  const { houseName, price, location, rating, photoUrl, description } = req.body;
+  const home = new Home(houseName, price, location, rating, photoUrl, description);
   home.save().then(() => {
-    console.log("Home Saved Successfully");
+    console.log('Home Saved successfully');
   });
 
   res.redirect("/host/host-home-list");
 };
 
 exports.postEditHome = (req, res, next) => {
-  const { id, houseName, price, location, rating, photoUrl, description } =
-    req.body;
-  const home = new Home(
-    houseName,
-    price,
-    location,
-    rating,
-    photoUrl,
-    description,
-    id
-  );
+  const { id, houseName, price, location, rating, photoUrl, description } = req.body;
+  const home = new Home(houseName, price, location, rating, photoUrl, description, id);
   home.save().then(result => {
-    console.log('Home Updated!',result);
-  })
+    console.log('Home updated ', result);
+  });
   res.redirect("/host/host-home-list");
 };
 
 exports.postDeleteHome = (req, res, next) => {
   const homeId = req.params.homeId;
-  console.log("Came to delete ", homeId);
-  Home.deleteById(homeId)
-    .then(() => {
-      res.redirect("/host/host-home-list");
-    })
-    .catch((error) => {
-      console.log("Error while Deleting", error);
-    });
+  console.log('Came to delete ', homeId);
+  Home.deleteById(homeId).then(() => {
+    res.redirect("/host/host-home-list");
+  }).catch(error => {
+    console.log('Error while deleting ', error);
+  })
 };
